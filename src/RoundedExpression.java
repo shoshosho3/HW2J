e47b1ffc5c9e1402ff0e1d0e8a7d37babcc5669c
maroun ailabouni <maron0522027924@gmail.com>
2022-05-24 16:42:27 +0300

    commit

--- src/RoundedExpression.java
+++ src/RoundedExpression.java
@@ -1,11 +1,24 @@
 public class RoundedExpression extends Expression {
+
+    private Expression expression;
+    private int digits;
+
+    public RoundedExpression(Expression expression, int digits) {
+        this.expression = expression;
+        this.digits = digits;
+    }
+
     @Override
     public double evaluate() {
-        return 0;
+        double num = 1;
+        for (int i = 0; i < digits; i++) {
+            num *= 10;
+        }
+        return (int) (expression.evaluate() * num) / num;
     }
 
     @Override
     public String toString() {
-        return null;
+        return expression.toString();
     }
 }
